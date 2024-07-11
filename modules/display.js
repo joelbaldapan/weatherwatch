7badba9a16924321d1c72c00be822c31a2dda56e
joelbaldapan <baldapan.joelangelo@gmail.com>
2024-07-12 05:32:52 +0800

    Refactor code by adding utils.js

--- modules/display.js
+++ modules/display.js
@@ -1,3 +1,5 @@
+import { cloneDetails, displayLoading, getDay, getDate } from "./utils.js";
+
 // DOM Manipulation
 
 const name = document.getElementById("name");
@@ -115,44 +117,6 @@ function reloadTextAnimations() {
     element.style.animation = "none";
     void element.offsetWidth;
     element.style.animation = "moveUp 1s forwards";
-  });
-}
-
-function cloneDetails(fix, original) {
-  const fixContainer = document.getElementById(fix);
-  const container = document.getElementById(original);
-  const clonedContainer = container.cloneNode(true);
-
-  clonedContainer.classList.add("gradient-overlay");
-  clonedContainer.classList.remove("gradient");
-
-  fixContainer.appendChild(clonedContainer);
-
-  setTimeout(() => {
-    clonedContainer.remove();
-  }, 2000);
-}
-
-function displayLoading(toggle) {
-  const loading = document.getElementById("loading");
-  const loadingImg = document.getElementById("loading-img");
-
-  loadingImg.src = "images/icons/loading.gif?a=" + Math.random();
-  loading.style.visibility = toggle;
-}
-
-function getDay(dateStr, locale, length) {
-  let date = new Date(dateStr);
-  return date.toLocaleDateString(locale, { weekday: length });
-}
-
-function getDate(dateStr, locale, length) {
-  let date = new Date(dateStr);
-  return date.toLocaleDateString(locale, {
-    weekday: length,
-    year: "numeric",
-    month: "long",
-    day: "numeric",
   });
 }
 
